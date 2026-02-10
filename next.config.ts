25a3c39340c1d7c4ef0ddee263ec8fe809d25c65
Captain-Kanak <kanakroy835@gmail.com>
2026-02-10 23:13:15 +0600

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -12,10 +12,10 @@ const nextConfig: NextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.API_URL}/api/:path*`,
-      },
+      // {
+      //   source: "/api/:path*",
+      //   destination: `${process.env.API_URL}/api/:path*`,
+      // },
       {
         source: "/:path*",
         destination: `${process.env.AUTH_URL}/:path*`,
