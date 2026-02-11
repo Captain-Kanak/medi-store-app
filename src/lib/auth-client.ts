62454cfec03cc9ac3f633bfb27621384a0eaae40
Captain-Kanak <kanakroy835@gmail.com>
2026-02-11 13:39:07 +0600

    modify auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,9 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
-const AUTH_URL = env.NEXT_PUBLIC_AUTH_URL;
+// const AUTH_URL = env.NEXT_PUBLIC_AUTH_URL;
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: AUTH_URL,
+  // baseURL: AUTH_URL,
 });
